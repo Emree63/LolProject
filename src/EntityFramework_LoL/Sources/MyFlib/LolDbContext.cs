6c2cd1d33923ee92ff31c3b2c511e96be8b1b281
emre.kartal <emre.kartal@etu.uca.fr>
2023-02-01 17:45:11 +0100

    Creating DataBase, Entity Champion and DBContext :white_check_mark:

---
+++ src/EntityFramework_LoL/Sources/MyFlib/LolDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFlib
+{
+    public class LolDbContext: DbContext
+    {
+        public DbSet<ChampionEntity> Champions { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(@"Data Source=DataBase.db");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChampionEntity>().HasData(new ChampionEntity("Akali"));
+        }
+
+    }
+}
---
+++ src/EntityFramework_LoL/Sources/MyFlib/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyFlib;
+
+Console.WriteLine("Hello, World!");
+
+using(var context = new LolDbContext())
+{
+    //var champions = context.Champions.where
+    context.SaveChangesAsync(); // or context.SaveChangesAsync
+}
