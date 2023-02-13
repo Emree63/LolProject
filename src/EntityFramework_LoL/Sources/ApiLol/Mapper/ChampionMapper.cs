6368c3d0ddef4678263d33c6b0a3fc1c82e44da8
Emre <Emre.kartal@etu.uca.fr>
2023-02-13 16:12:04 +0100

    Creation of the DTO and Mapper for the Skin class, addition of new attributes for the championDto and complete put and delete function for the champion controller

--- src/EntityFramework_LoL/Sources/ApiLol/Mapper/ChampionMapper.cs
+++ src/EntityFramework_LoL/Sources/ApiLol/Mapper/ChampionMapper.cs
@@ -11,15 +11,15 @@ namespace ApiLol.Mapper
             {
                 Name = champion.Name,
                 Bio = champion.Bio,
+                Class = champion.Class.ToDto(),
+                Icon = champion.Icon,
+                Image = champion.Image.Base64
             };
         }
 
         public static Champion ToModel(this ChampionDto championDto)
         {
-            return new Champion(championDto.Name)
-            {
-                Bio = championDto.Bio,
-            };
+            return new Champion(championDto.Name, championDto.Class.ToModel(), championDto.Icon, championDto.Image,championDto.Bio);
         }
 
     }
---
+++ src/EntityFramework_LoL/Sources/ApiLol/Mapper/enums/ChampionClassMapper.cs
@@ -0,0 +1,17 @@
+﻿using DTO;
+using Model;
+
+namespace ApiLol.Mapper
+{
+    public static class ChampionClassMapper
+    {
+        public static ChampionClassDto ToDto(this ChampionClass championClass)
+        {
+            return (ChampionClassDto) championClass;
+        }
+        public static ChampionClass ToModel(this ChampionClassDto championClass)
+        {
+            return (ChampionClass) championClass;
+        }
+    }
+}
